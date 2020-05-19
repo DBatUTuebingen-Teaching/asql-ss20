6e5eae0c2ef94d043c5ebfc8cd3085e8c33cdaed
Christian Duta <christian.duta@informatik.uni-tuebingen.de>
2020-05-19 17:13:19 +0200

    Add assignment #4

---
+++ assignments/assignment04/assignment04.sql
@@ -0,0 +1,36 @@
+-- 1. 
+
+SELECT p.a, p.b * 2, p.c, p.d, p.e, p.f
+FROM   (VALUES 
+  (1,'2'::money,4   ,42+1::real,1::real,NULL),
+  (2,'5.72'    ,1.32,2         ,2      ,NULL),
+  (3,'2'::money,5.77,3         ,3      ,NULL)
+) AS p(a,b,c,d,e,f)
+WHERE  p.c < 5.5;
+
+SELECT p.a, p.b * 2, p.c, p.d, p.e, p.f
+FROM   p AS p
+WHERE  p.c < 5.5;
+
+-------------------------------------------------------------------------------
+
+-- 2.
+
+CREATE TABLE allcards_json (
+  data json
+);
+
+\copy allcards_json FROM 'path/to/AllCards.json/here';
+
+DROP TABLE mtj CASCADE;
+CREATE TABLE mtj (
+  name      text PRIMARY KEY,
+  mana_cost text,
+  cmc       numeric,
+  type      text,
+  text      text,
+  power     text,
+  toughness text
+);
+
+
